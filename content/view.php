f09b9c2460d964767ed488d3572372e7f88c357c
Javad Evazzadeh <J.Evazzadeh@live.com>
2018-11-13 15:47:24 +0330

    init again from dashSample

--- content/view.php
+++ content/view.php
@@ -5,25 +5,13 @@ class view
 {
 	public static function config()
 	{
-		\dash\data::site_title(T_("Khadije Charity"));
-		\dash\data::site_desc(T_("Executor of first pilgrimage to the Ahl al-Bayt"));
-		\dash\data::site_slogan(T_('Karbala'). ' - '. T_('Mashhad'). ' - '. T_('Qom'));
-
+		// define default value for global
+		\dash\data::site_title(T_("Dash"));
+		\dash\data::site_desc(T_("Free PHP Framework & CMS!"));
+		\dash\data::site_slogan(T_("The simple framework for php programmers ;)"));
 		\dash\data::page_desc(\dash\data::site_desc(). ' | '. \dash\data::site_slogan());
-
-		\dash\data::page_copyright(
-			T_('© :year :site. All right reserved.',
-			[
-				'year' => \dash\date::tdate(false, "year", true),
-				'site' => "<a href='". \dash\url::base(). "' title='". \dash\data::site_desc() ."'>". \dash\data::site_title(). "</a>"
-			]
-		));
-
-		\dash\data::hadithList(\lib\app\need::active_list('hadith'));
-
-
-		// for pushstate of main page
-		// \dash\data::template_xhr('content/main/layout-xhr.html');
+		\dash\data::include_css(false);
+		\dash\data::bodyclass(null);
 	}
 }
 ?>
