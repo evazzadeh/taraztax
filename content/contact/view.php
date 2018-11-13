b3b7236c2c701ca1643ac3d70ff598ef35cfa916
Javad Evazzadeh <J.Evazzadeh@live.com>
2018-11-13 16:20:10 +0330

    add contact

---
+++ content/contact/view.php
@@ -0,0 +1,15 @@
+<?php
+namespace content\contact;
+
+
+class view
+{
+
+	public static function config()
+	{
+		\dash\data::page_title(T_("Contact Us"));
+		\dash\data::page_desc(T_("We do our best to improve our service quality."));
+		\dash\data::bodyclass('unselectable');
+	}
+}
+?>
