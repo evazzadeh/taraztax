e982c737d512a9c6f508262f872839c4438a7c8b
Reza <rm.biqarar@gmail.com>
2018-12-27 18:57:30 +0330

    user master ticket

--- content/contact/view.php
+++ content/contact/view.php
@@ -2,7 +2,7 @@
 namespace content\contact;
 
 
-class view
+class view extends \content_support\ticket\contact_ticket\view
 {
 
 	public static function config()
@@ -10,6 +10,7 @@ class view
 		\dash\data::page_title(T_("Contact Us"));
 		\dash\data::page_desc(T_("We do our best to improve our service quality."));
 		\dash\data::bodyclass('unselectable');
+		self::codeurl();
 	}
 }
 ?>
