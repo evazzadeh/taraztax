5f20242ae3e35a79659ac8a95930e26c6de1a1f0
Reza <rm.biqarar@gmail.com>
2019-01-01 13:54:09 +0330

    set tg and api

--- includes/option/option.php
+++ includes/option/option.php
@@ -24,10 +24,26 @@ self::$config['enter']['force_verify'] = false;
 
 
 
-self::$sms['kavenegar']['status']             = true;
-self::$sms['kavenegar']['apikey']             = '44326537795238302B5342544364344E70426770507076752F6553544C674E66';
-self::$sms['kavenegar']['header']             = null;
-self::$sms['kavenegar']['footer']             = 'TarazTax.ir';
+self::$sms['kavenegar']['status'] = true;
+self::$sms['kavenegar']['apikey'] = '44326537795238302B5342544364344E70426770507076752F6553544C674E66';
+self::$sms['kavenegar']['debug']  = null;
+self::$sms['kavenegar']['header'] = null;
+self::$sms['kavenegar']['footer'] = 'TarazTax.ir';
+self::$sms['kavenegar']['iran']   = null;
+self::$sms['kavenegar']['line']   = '10006660066600';
+self::$sms['kavenegar']['one']    = true;
+
+
+
+self::$social['telegram']['status']     = true;
+self::$social['telegram']['bot']        = 'TarazTaxBot';
+self::$social['telegram']['hookFolder'] = 'HelloSupporter';
+self::$social['telegram']['token']      = '677212024:AAHaGzycIDfCVBuPKXtJZqukuZqMlRA3CGY';
+self::$social['telegram']['debug']      = true;
+
+
+
+
 
 
 ?>
