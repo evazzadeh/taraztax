a595c3a93c1f015b984d0fe9ffc11f81d4ac549b
Reza <rm.biqarar@gmail.com>
2019-01-01 13:42:29 +0330

    set api key

--- includes/option/option.php
+++ includes/option/option.php
@@ -22,4 +22,12 @@ self::$config['site']['slogan']        = "be relax with us in your tax problems!
 self::$config['debug']                 = true;
 self::$config['enter']['force_verify'] = false;
 
+
+
+self::$sms['kavenegar']['status']             = true;
+self::$sms['kavenegar']['apikey']             = '44326537795238302B5342544364344E70426770507076752F6553544C674E66';
+self::$sms['kavenegar']['header']             = null;
+self::$sms['kavenegar']['footer']             = 'TarazTax.ir';
+
+
 ?>
