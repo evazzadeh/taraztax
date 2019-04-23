7cc7b958e5080c334bf4eec9ece0cef1df454d68
Javad Evazzadeh <J.Evazzadeh@live.com>
2019-04-23 12:41:49 +0430

    bugfix preview

--- includes/languages/trans_project.php
+++ includes/languages/trans_project.php
@@ -4,81 +4,51 @@ class twigTransTerms
  private function transtext()
  {
 
-	//---------------------public_html/static/siftal/fonts/siftal/icons-reference.html
+	//-------------------------------------------------------content\blog\display.html
+	echo T_("View your current location and navigate to parent of it");               // Line 6
 
-	//--------------------------------------public_html/static/siftal/js/siftal.min.js
+	//----------------------------------------------------content\contact\display.html
+	echo T_("We do our best to improve our service quality. So, knowing your valuable comments will help us in this way.");// Line 10
+	echo T_("Iran");                                                                  // Line 44
+	echo T_("Qom");                                                                   // Line 210
+	echo T_("Name");                                                                  // Line 138
+	echo T_("Full Name");                                                             // Line 139
+	echo T_("Mobile");                                                                // Line 26
+	echo T_("Please enter valid mobile number. `:val` is incorrect");                 // Line 146
+	echo T_("Email");                                                                 // Line 27
+	echo T_("Your Message");                                                          // Line 155
+	echo T_("Send");                                                                  // Line 161
 
-	//-----------------------public_html/static/siftal/js/highcharts/highcharts.min.js
+	//-------------------------------------------------------content\home\display.html
+	echo T_("Certificate");                                                           // Line 245
+	echo T_("Secured by Arvancloud");                                                 // Line 246
+	echo T_("Arvancloud");                                                            // Line 246
+	echo T_("Last news");                                                             // Line 290
 
-	//-----------------------------------public_html/static/siftal/js/highlight.min.js
-
-	//--------------------------------------public_html/static/siftal/js/error_page.js
-
-	//----------------------------------------------------content/template/footer.html
-	echo T_("Enter");                                                                 // Line 7
-	echo T_("About");                                                                 // Line 42
-	echo T_("Contact");                                                               // Line 26
-	echo T_("Mobile");                                                                // Line 170
-	echo T_("Email");                                                                 // Line 179
-	echo T_("Developed by Ermile");                                                   // Line 50
-
-	//---------------------------------------------------content/template/dafault.html
-	echo T_("For share via social networks");                                         // Line 42
-	echo T_("News Code");                                                             // Line 42
-	echo T_("Download");                                                              // Line 80
-	echo T_("PDF");                                                                   // Line 80
-	echo T_("Comment");                                                               // Line 126
-	echo T_("Name");                                                                  // Line 79
-	echo T_("Full Name");                                                             // Line 80
-	echo T_("Please enter valid mobile number. `:val` is incorrect");                 // Line 88
-	echo T_("Your Message");                                                          // Line 102
-	echo T_("Send");                                                                  // Line 108
-
-	//----------------------------------------------------content/template/header.html
-
-	//--------------------------------------------------------content/main/layout.html
+	//--------------------------------------------------------content\main\layout.html
 	echo T_("Next →");                                                                // Line 38
 	echo T_("← Back");                                                                // Line 38
 	echo T_("Skip");                                                                  // Line 38
 	echo T_("Done");                                                                  // Line 38
 
-	//----------------------------------------------------content/contact/display.html
-	echo T_("We do our best to improve our service quality. So, knowing your valuable comments will help us in this way.");// Line 10
-	echo T_("Iran");                                                                  // Line 44
-	echo T_("Qom");                                                                   // Line 103
+	//-----------------------------------------------------content\old\v1\display.html
+	echo T_("About");                                                                 // Line 25
+	echo T_("Shahid Mahdi Imani");                                                    // Line 110
+	echo T_("Do you know?");                                                          // Line 188
+	echo T_("Family trip signup");                                                    // Line 84
+	echo T_("Group trip signup");                                                     // Line 72
+	echo T_("Donate");                                                                // Line 14
+	echo T_("Mashhad");                                                               // Line 218
+	echo T_("Karbala");                                                               // Line 226
+	echo T_("license and supporters");                                                // Line 240
 
-	//------------------------------------------------------content/old/v2/footer.html
-	echo T_("Signup");                                                                // Line 8
-	echo T_("Misson & Target");                                                       // Line 22
-	echo T_("Vision");                                                                // Line 23
-	echo T_("Honors");                                                                // Line 24
-	echo T_("FAQ");                                                                   // Line 25
-	echo T_("Donate");                                                                // Line 86
-	echo T_("Doners");                                                                // Line 34
-	echo T_("Register for group trip");                                               // Line 42
+	//-----------------------------------------------------content\old\v2\dafault.html
 
-	//------------------------------------------------------content/old/v2/layout.html
-
-	//-----------------------------------------------------content/old/v2/dafault.html
-	echo T_("View your current location and navigate to parent of it");               // Line 7
-
-	//-----------------------------------------------------content/old/v2/display.html
-	echo T_("Group trip signup");                                                     // Line 85
+	//-----------------------------------------------------content\old\v2\display.html
 	echo T_("Click to go to download page");                                          // Line 79
 	echo T_("Download love story book");                                              // Line 80
-	echo T_("Shahid Mahdi Imani");                                                    // Line 69
-	echo T_("Do you know?");                                                          // Line 80
-	echo T_("Mashhad");                                                               // Line 111
-	echo T_("Karbala");                                                               // Line 119
-	echo T_("license and supporters");                                                // Line 133
-	echo T_("Certificate");                                                           // Line 91
-	echo T_("Secured by Arvancloud");                                                 // Line 92
-	echo T_("Arvancloud");                                                            // Line 92
-	echo T_("Last news");                                                             // Line 133
 
-	//------------------------------------------------------content/old/v2/header.html
-
-	//--------------------------------------------------content/old/v2/donateForm.html
+	//--------------------------------------------------content\old\v2\donateForm.html
 	echo T_("List of last success donations");                                        // Line 90
 	echo T_("Requested");                                                             // Line 118
 	echo T_("Toman");                                                                 // Line 200
@@ -109,12 +79,40 @@ class twigTransTerms
 	echo T_("Spending method");                                                       // Line 239
 	echo T_("Please select one item");                                                // Line 254
 
-	//-----------------------------------------------------content/old/v1/display.html
-	echo T_("Family trip signup");                                                    // Line 84
+	//------------------------------------------------------content\old\v2\footer.html
+	echo T_("Enter");                                                                 // Line 9
+	echo T_("Signup");                                                                // Line 8
+	echo T_("Misson & Target");                                                       // Line 22
+	echo T_("Vision");                                                                // Line 23
+	echo T_("Honors");                                                                // Line 24
+	echo T_("FAQ");                                                                   // Line 25
+	echo T_("Contact");                                                               // Line 26
+	echo T_("Doners");                                                                // Line 34
+	echo T_("Register for group trip");                                               // Line 42
+	echo T_("Developed by Ermile");                                                   // Line 35
 
-	//-------------------------------------------------------content/blog/display.html
+	//------------------------------------------------------content\old\v2\header.html
 
-	//-------------------------------------------------------content/home/display.html
+	//------------------------------------------------------content\old\v2\layout.html
+
+	//---------------------------------------------------content\template\dafault.html
+	echo T_("For share via social networks");                                         // Line 42
+	echo T_("News Code");                                                             // Line 42
+	echo T_("Download");                                                              // Line 80
+	echo T_("PDF");                                                                   // Line 80
+	echo T_("Comment");                                                               // Line 126
+
+	//----------------------------------------------------content\template\footer.html
+
+	//----------------------------------------------------content\template\header.html
+
+	//---------------------public_html\static\siftal\fonts\siftal\icons-reference.html
+
+	//--------------------------------------public_html\static\siftal\js\error_page.js
+
+	//-----------------------public_html\static\siftal\js\highcharts\highcharts.min.js
+
+	//-----------------------------------public_html\static\siftal\js\highlight.min.js
 
  }
 }
